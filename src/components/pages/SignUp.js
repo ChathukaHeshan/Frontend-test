ea5b7be3420a84d8f2105de67b84bea9f7edf5cc
ChathukaHeshan <86787095+ChathukaHeshan@users.noreply.github.com>
2021-09-10 10:28:29 +0530

    new

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+import SignIn from '../SignIn';
+
+export default function SignUp() {
+  return(
+    <>
+    <SignIn/>
+    <Footer/>
+  </>
+     
+  );
+}
